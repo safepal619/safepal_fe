34db71dc7c4b017049bb56c0ddc3d9a2aba6e836
safepal619 <safepal619@gmail.com>
2024-06-24 02:30:03 +0100

    change base url

--- Root.js
+++ Root.js
@@ -1,3 +1,4 @@
-export const BaseUrl = "http://localhost:3000/api"
-export const AdminBaseUrl = "http://localhost:3000/"
-// export const BaseUrl = "https://safepal-bg.onrender.com/api"
+// export const BaseUrl = "http://localhost:3000/api"
+// export const AdminBaseUrl = "http://localhost:3000/"
+export const AdminBaseUrl = "https://safepal-bg.onrender.com/"
+export const BaseUrl = "https://safepal-bg.onrender.com/api"
